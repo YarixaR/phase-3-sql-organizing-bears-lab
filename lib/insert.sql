94b3be9a3166b87c300744be52569901790a6781
Yarixa Rodriguez <yarixarodriguez94@gmail.com>
2022-08-11 21:39:20 -0400

    commit tree 45bac78d1142c8639f60bab3e0c2db4c69d16262

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr.Chocolate", 3, "M", "Brown", "Calm", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 4, "F", "White", "Angry", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 35, "F", "Brown", "Bitter", 0);
+INSERT INTO bears VALUES (4, "Sargeant Brown", 15, "M", "Brown", "Salty", 1);
+INSERT INTO bears VALUES (5, "Melissa", 1, "F", "Black", "Sweet", 1);
+INSERT INTO bears VALUES (6, "Grinch", 50, "M", "Green", "Grinchy", 1);
+INSERT INTO bears VALUES (7, "Wendy", 100, "F", "White", "Stoic", 0);
+INSERT INTO bears VALUES (8, null, 10, "M", "Grey", "Aggressive", 0);
